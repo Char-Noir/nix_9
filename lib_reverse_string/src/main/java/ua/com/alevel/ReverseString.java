f2c37171d2a9ad36a58ca38080690a0b6e72dc80
AnnYat <AnnYat@BitBucket.com>
2021-10-19 14:28:21 +0300

    Add reverse substring and by indexes

--- lib_reverse_string/src/main/java/ua/com/alevel/ReverseString.java
+++ lib_reverse_string/src/main/java/ua/com/alevel/ReverseString.java
@@ -25,4 +25,25 @@ public class ReverseString {
             return reverse(src);
         }
     }
+
+    public static String reverse(String src, String dest) {
+        int index = src.indexOf(dest);
+        if (index == -1) {
+            return src;
+        }
+        String reverse = reverse(dest);
+        return src.replace(dest, reverse);
+    }
+
+    public static String reverse(String src, int firstIndex, int lastIndex) {
+        return reverse(src, firstIndex, lastIndex, false);
+    }
+
+    public static String reverse(String src, int firstIndex, int lastIndex, boolean byWords) {
+        String dest = src.substring(firstIndex, lastIndex);
+        String reverseDest = reverse(dest, byWords);
+        StringBuilder buf = new StringBuilder(src);
+
+        return buf.replace(firstIndex, lastIndex, reverseDest).toString();
+    }
 }
