a506943af32ad8733f10945c110bd64d9220db8b
AnnYat <AnnYat@BitBucket.com>
2021-10-19 14:30:30 +0300

    Add classes for subroutines: reverse substring and by indexes

---
+++ hw_2_strings/src/main/java/ua/com/alevel/subroutines/ReverseByIndexes.java
@@ -0,0 +1,48 @@
+package ua.com.alevel.subroutines;
+
+import ua.com.alevel.ReverseString;
+import ua.com.alevel.console.ConsoleSubroutine;
+
+import java.io.BufferedReader;
+
+public class ReverseByIndexes extends ConsoleSubroutine {
+
+    public final String LONG_DESCRIPTION = "Возвращает строку, в которой реверсирована подстрока, указанная индексами начала и конца. Ex: \"Hello world\" , \"world\" -> \"Hello dlrow \"";
+    public final String SHORT_DESCRIPTION = "Реверсирует подстроку в строке.";
+
+    public final String EXPECTED_INPUT = "Строка и индексы начала и конца подстроки, которую будут реверсировать, разделенные символом зяпятой";
+
+    @Override
+    public String getShortDescription() {
+        return SHORT_DESCRIPTION;
+    }
+
+    @Override
+    public String getLongDescription() {
+        return LONG_DESCRIPTION;
+    }
+
+    @Override
+    public String getExpectedInput() {
+        return EXPECTED_INPUT;
+    }
+
+    @Override
+    public void run(BufferedReader br) {
+        String[] strings;
+        String reversed = "";
+
+        try {
+            String string = br.readLine();
+            strings = string.split(",");
+            reversed = ReverseString.reverse(strings[0], Integer.parseInt(strings[1]), Integer.parseInt(strings[2]));
+        } catch (Exception e) {
+            System.err.println(ERROR_MESSAGE);
+            strings = new String[]{"", "", ""};
+            e.printStackTrace();
+        }
+
+        System.out.println(new StringBuilder("Результат задания: ").append(strings[0]).append(" with ").append(strings[1]).append(",").append(strings[2]).append(" -> ").append(reversed).append("\n"));
+    }
+}
+
