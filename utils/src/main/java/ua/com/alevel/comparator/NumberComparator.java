819e0ae6ee29879d40dd199babeafe8d2615cfd8
AnnYat <AnnYat@BitBucket.com>
2021-11-06 20:59:16 +0200

    Add Number Compataror for comparing all number, which can be represented in BigDecimal

---
+++ utils/src/main/java/ua/com/alevel/comparator/NumberComparator.java
@@ -0,0 +1,11 @@
+package ua.com.alevel.comparator;
+
+import java.math.BigDecimal;
+import java.util.Comparator;
+
+public class NumberComparator <T extends Number> implements Comparator<T> {
+
+    public int compare( T a, T b ) throws ClassCastException {
+        return new BigDecimal(String.valueOf(a)).compareTo(new BigDecimal(String.valueOf(b))) ;
+    }
+}
