19acc346fda0c80ec72226bfc226eedc72f37b70
AnnYat <AnnYat@BitBucket.com>
2021-11-14 21:27:22 +0200

    fix line spacing

--- utils/src/main/java/ua/com/alevel/comparator/NumberComparator.java
+++ utils/src/main/java/ua/com/alevel/comparator/NumberComparator.java
@@ -3,9 +3,9 @@ package ua.com.alevel.comparator;
 import java.math.BigDecimal;
 import java.util.Comparator;
 
-public class NumberComparator <T extends Number> implements Comparator<T> {
+public class NumberComparator<T extends Number> implements Comparator<T> {
 
-    public int compare( T a, T b ) throws ClassCastException {
-        return new BigDecimal(String.valueOf(a)).compareTo(new BigDecimal(String.valueOf(b))) ;
+    public int compare(T a, T b) throws ClassCastException {
+        return new BigDecimal(String.valueOf(a)).compareTo(new BigDecimal(String.valueOf(b)));
     }
 }
