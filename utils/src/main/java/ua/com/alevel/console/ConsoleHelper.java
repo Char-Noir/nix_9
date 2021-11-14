18e27a758aacac58b88f9f051fe728cec5297c2a
AnnYat <AnnYat@BitBucket.com>
2021-11-14 21:32:43 +0200

    Add all new features to runConsoleApp ToDo: make full reflection app

--- utils/src/main/java/ua/com/alevel/console/ConsoleHelper.java
+++ utils/src/main/java/ua/com/alevel/console/ConsoleHelper.java
@@ -1,24 +1,22 @@
 package ua.com.alevel.console;
 
+import ua.com.alevel.console.annotation.ListSubroutineAnnotationAnalyze;
+import ua.com.alevel.console.responce.ConsoleResponceSubroutine;
+import ua.com.alevel.console.responce.Responce;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Locale;
 
 public class ConsoleHelper {
 
     public static final String DELIMETR = "##########################################################";
 
     public static void runConsoleApplication(List<ConsoleSubroutine> subroutines) {
-
-        StringBuilder programDescription = new StringBuilder("Hello, dude!\n This program allow you to perform some actions by your choice.\n Just pick number from the list bellow. For exit type something else\n");
-
-        for (int i = 0; i < subroutines.size(); i++) {
-            programDescription.append(i + 1).append(". ").append(subroutines.get(i).getShortDescription()).append("\n");
-        }
-
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
-            System.out.println(programDescription);
+            printDescriptions(subroutines);
 
             for (String line = br.readLine(); line != null; line = br.readLine()) {
                 if (!line.matches("[0-9]+")) {
@@ -35,7 +33,7 @@ public class ConsoleHelper {
                         System.out.println("Ожидаемый ввод: " + subroutine.getExpectedInput());
                     }
                     subroutine.run(br);
-                    System.out.println(programDescription);
+                    printDescriptions(subroutines);
                 } else {
                     System.out.println("Хорошего дня!😀");
                     break;
@@ -45,6 +43,74 @@ public class ConsoleHelper {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
+
+    public static void runConsoleResponseApplication(List<ConsoleResponceSubroutine> subroutines) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            String redirect = null;
+            ListSubroutineAnnotationAnalyze.runFirstSubroutine(subroutines.toArray(new Subroutine[0]), bufferedReader);
+            while (true) {
+                String line = null;
+                if (redirect == null) {
+                    printDescriptions(subroutines);
+                    line = bufferedReader.readLine();
+                    if (line == null) {
+                        break;
+                    }
+                }
+                if ((line != null && line.matches("[0-9]+")) && redirect == null) {
+                    int choice = Integer.parseInt(line) - 1;
+                    if (choice >= 0 && choice < subroutines.size()) {
+                        ConsoleResponceSubroutine subroutine = subroutines.get(choice);
+                        redirect = runSubroutine(bufferedReader, String.valueOf(choice + 1), subroutine);
+                    } else {
+                        System.out.println("Хорошего дня!😀");
+                        break;
+                    }
+                } else {
+                    if (redirect != null) {
+                        line = redirect;
+                    }
+                    String name = line;
+                    line = line.toLowerCase(Locale.ROOT);
+                    boolean isWorked = false;
+                    for (ConsoleResponceSubroutine subroutine :
+                            subroutines) {
+                        if (line.contentEquals(subroutine.getSubroutineURL())) {
+                            redirect = runSubroutine(bufferedReader, name, subroutine);
+                            isWorked = true;
+                            break;
+                        }
+                    }
+                    if (!isWorked) {
+                        System.out.println("Хорошего дня!😀");
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String runSubroutine(BufferedReader bufferedReader, String path, ConsoleResponceSubroutine subroutine) {
+        System.out.println("Задание №" + (path));
+        System.out.println(DELIMETR);
+        System.out.println(subroutine.getLongDescription());
+        if (subroutine.getExpectedInput() != "") {
+            System.out.println("Ожидаемый ввод: " + subroutine.getExpectedInput());
+        }
+        Responce responce = subroutine.run(bufferedReader);
+        return responce.getRedirectTo();
+    }
+
+    private static void printDescriptions(List<? extends Subroutine> subroutines) {
+        StringBuilder programDescription = new StringBuilder("Hello, dude!\n This program allow you to perform some actions by your choice.\n Just pick number from the list bellow. For exit type something else\n");
+        for (int i = 0; i < subroutines.size(); i++) {
+            programDescription.append(i + 1).append(". ").append(subroutines.get(i).getShortDescription()).append("\n");
+        }
+        System.out.println(programDescription);
+    }
+
+
 }
